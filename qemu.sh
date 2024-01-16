320487daf2073fc6d3a32d65560f567e9f2601d0
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-16 21:15:28 +0100

    chore: qemu.sh now boots the disk image

--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-qemu-system-i386 -m 128 -no-reboot -drive format=raw,media=disk,file=bootloader/bin/boot.bin -serial stdio -smp 1 -usb -vga std
+#qemu-system-i386 -m 128 -no-reboot -drive format=raw,media=disk,file=bootloader/bin/boot.bin -serial stdio -smp 1 -usb -vga std
+qemu-system-i386 -m 128 -no-reboot -drive format=raw,media=disk,file=CacaoOs_floppy.img -serial stdio -smp 1 -usb -vga std
