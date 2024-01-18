034ba0bda7fd0b564952199b9404fc81ad7b9c2d
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-18 21:33:47 +0100

    feat: kill global offset table for all artifacts.

--- cacaoKernel/build.sh
+++ cacaoKernel/build.sh
@@ -1,3 +1,4 @@
 #
-clang -m32 -ffreestanding -c src/kernel.c -o bin/kernel.o
-ld -m elf_i386 -o bin/kernel.bin -Ttext 0x1000 bin/kernel.o --oformat binary
+nasm src/entry.asm -f elf -o bin/entry.o 
+clang -m32 -ffreestanding -fno-pie -c src/kernel.c -o bin/kernel.o
+ld -m elf_i386 -no-pie -o bin/kernel.bin -Ttext 0x1000 bin/entry.o ../drivers/bin/vga_textmode.o bin/kernel.o --oformat binary -v
