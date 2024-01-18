b02d7dae9390a0af04671ec11591ec4db6810eef
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-18 21:55:17 +0100

    feat: add default screen width and height as constants

--- drivers/vga/vga_textmode.h
+++ drivers/vga/vga_textmode.h
@@ -1,6 +1,10 @@
 #ifndef __VGA_TEXTMODE_H__
 #define __VGA_TEXTMODE_H__
 
+#define SCREEN_WIDTH        80
+#define SCREEN_HEIGHT       25
+
+void video_test_writes();
 void video_buffer_simple_write();
 
 #endif
