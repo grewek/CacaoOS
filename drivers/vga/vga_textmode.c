967db50a3be91d2eb61ea05efce18fc56b3c0eba
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-18 21:35:24 +0100

    feat: add skeleton vga_textmode driver

---
+++ drivers/vga/vga_textmode.c
@@ -0,0 +1,14 @@
+#include "vga_textmode.h"
+//TODO(Kay): We got repetition here so we need a general type header like stdint cacaoTypes.h ? :)
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef signed char i8;
+typedef signed char i16;
+typedef signed char i32;
+
+void video_buffer_simple_write() {
+    u16 *videoMemory = (u16 *)0xb8000;
+    *videoMemory = (u16)0x0f << 8 | (u16) 'A';
+}
---
+++ drivers/vga/vga_textmode.h
@@ -0,0 +1,6 @@
+#ifndef __VGA_TEXTMODE_H__
+#define __VGA_TEXTMODE_H__
+
+void video_buffer_simple_write();
+
+#endif
