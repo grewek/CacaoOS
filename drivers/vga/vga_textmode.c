7a44a35a7151983452493bf20c5814f0dfa27f64
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-18 21:54:36 +0100

    feat: add function to calculate linear address from given coordinates

--- drivers/vga/vga_textmode.c
+++ drivers/vga/vga_textmode.c
@@ -8,6 +8,10 @@ typedef signed char i8;
 typedef signed char i16;
 typedef signed char i32;
 
+//Calculates the coordinate of the character 
+u16 GenerateVideoCoordinate(u16 x, u16 y) {
+    return y * SCREEN_WIDTH + x;    
+}
 void video_buffer_simple_write() {
     u16 *videoMemory = (u16 *)0xb8000;
     *videoMemory = (u16)0x0f << 8 | (u16) 'A';
