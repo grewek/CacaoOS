a817de72c0a019687f586eb2eb14a465bb2f205b
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-19 23:43:55 +0100

    feat: add vga color attribute enum

--- drivers/vga/vga_textmode.h
+++ drivers/vga/vga_textmode.h
@@ -13,6 +13,26 @@ typedef signed int bool;
 #define SCREEN_WIDTH        80
 #define SCREEN_HEIGHT       25
 
+typedef enum ColorAttrib {
+    BLACK = 0x00,
+    BLUE = 0x01,
+    GREEN = 0x02,
+    CYAN = 0x03,
+    RED = 0x04,
+    MAGENTA = 0x05,
+    BROWN = 0x06,
+    LIGHT_GRAY = 0x07,
+
+    DARK_GRAY = 0x08,
+    LIGHT_BLUE = 0x09,
+    LIGHT_GREEN = 0x0a,
+    LIGHT_CYAN = 0x0b,
+    LIGHT_RED = 0x0c,
+    PINK = 0x0d,
+    YELLOW = 0x0e,
+    WHITE = 0x0f,
+} ColorAttrib;
+
 void video_test_writes();
 void video_buffer_simple_write();
 
