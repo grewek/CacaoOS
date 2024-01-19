4c3232a63796bfd27d0b0d67076ea93004049cf1
Kay Grewe <Kay.Grewe@protonmail.com>
2024-01-19 23:43:16 +0100

    chore: move basic types into vga_textmode.h

--- drivers/vga/vga_textmode.h
+++ drivers/vga/vga_textmode.h
@@ -1,6 +1,15 @@
 #ifndef __VGA_TEXTMODE_H__
 #define __VGA_TEXTMODE_H__
 
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef signed char i8;
+typedef signed char i16;
+typedef signed char i32;
+typedef signed int bool;
+
 #define SCREEN_WIDTH        80
 #define SCREEN_HEIGHT       25
 
