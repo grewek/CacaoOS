a173af7bf852bf82ef5391f28e58e7d7c4d40dd5
Kay Grewe <Kay.Grewe@protonmail.com>
2024-02-04 11:21:44 +0100

    refactor: add bin/entry into the linking process, add compiler flags

--- cacaoKernel/build.sh
+++ cacaoKernel/build.sh
@@ -1,4 +1,5 @@
 #
 nasm src/entry.asm -f elf -o bin/entry.o 
-clang -m32 -ffreestanding -fno-pie -c src/kernel.c -o bin/kernel.o
-ld -m elf_i386 -no-pie -o bin/kernel.bin -Ttext 0x1000 bin/entry.o ../drivers/bin/vga_textmode.o bin/kernel.o --oformat binary -v
+clang -m32 -fno-stack-protector -ffreestanding -fno-pie -c src/kernel.c -o bin/kernel.o
+#ld -m elf_i386 -no-pie -o bin/kernel.bin -Ttext 0x1000 bin/entry.o ../drivers/bin/vga_textmode.o bin/kernel.o --oformat binary -v
+ld -m elf_i386 -no-pie -o bin/kernel.bin -T "linker.ld" bin/entry.o ../drivers/bin/vga_textmode.o bin/kernel.o --oformat binary -v
