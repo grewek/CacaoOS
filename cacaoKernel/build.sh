12e223128b52ff3a0546df1ff976227dae60a6e3
Kay Grewe <Kay.Grewe@protonmail.com>
2024-02-04 11:31:08 +0100

    chore: add comment about linking with the entry point

--- cacaoKernel/build.sh
+++ cacaoKernel/build.sh
@@ -1,4 +1,4 @@
-#
+#IMPORTANT(Kay): forgot to link with the entrypoint...this should always be linked no matter what !
 nasm src/entry.asm -f elf -o bin/entry.o 
 clang -m32 -fno-stack-protector -ffreestanding -fno-pie -c src/kernel.c -o bin/kernel.o
 #ld -m elf_i386 -no-pie -o bin/kernel.bin -Ttext 0x1000 bin/entry.o ../drivers/bin/vga_textmode.o bin/kernel.o --oformat binary -v
