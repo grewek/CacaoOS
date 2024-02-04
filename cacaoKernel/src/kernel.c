6c8b4e62ff317ae6aa750df9d03458dc96ef2e9a
Kay Grewe <Kay.Grewe@protonmail.com>
2024-02-04 12:10:12 +0100

    test: testing the new writing and clearing functionality in the kernel

--- cacaoKernel/src/kernel.c
+++ cacaoKernel/src/kernel.c
@@ -10,6 +10,7 @@ typedef signed char i32;
 
 
 void main() {
-    kernel_putstr("Hello World !", 13);
-    //test_kputstr();
+    Clear();
+    Kernel_Print("[INFO]\tCacao OS Kernel Booted !\n");
+    Kernel_Print("[TEST]\tThis line will overflow the screen size by multiple characters but kernel_print will just increase the line number and print the rest of the text to the next line !");
 }
