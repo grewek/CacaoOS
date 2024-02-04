cbe3c49c6f58c6d5fe6bc214f4ccbc12cfe3650a
Kay Grewe <Kay.Grewe@protonmail.com>
2024-02-04 11:21:06 +0100

    chore: fix spelling error, add security comment, fix disk size

--- build_all.sh
+++ build_all.sh
@@ -7,7 +7,7 @@ echo "Building Bootloader"
 cd bootloader
 . build.sh
 
-#This should be static at 512 bytes maybe we should make a error state ?
+#This should be static at 512 bytes maybe we should make an error state ?
 bootloader_size=$(stat -c %s "bin/boot.bin")
 
 #Build the drivers
@@ -32,7 +32,7 @@ os_size=$((bootloader_size + kernel_size))
 
 echo "The complete image size is $os_size"
 
-floppy_size=1509949
+floppy_size=1409949
 fill_size=$((floppy_size- os_size))
 
 echo "Creating Image from boot.bin and kernel.bin"
@@ -40,4 +40,5 @@ cat bootloader/bin/boot.bin cacaoKernel/bin/kernel.bin > CacaoOs_floppy.img
 
 # Enable when we get Bochs to work ?
 echo "The image will be filled with $fill_size bytes of zero data"
+# dd without any security checks is probably a bad idea as well...
 dd if=/dev/zero bs="$fill_size" count=1 >> CacaoOs_floppy.img
