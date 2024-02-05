a06a0d6127f5c0c0825106ee4455232d62279571
Kay Grewe <Kay.Grewe@protonmail.com>
2024-02-05 21:56:18 +0100

    chore: compile io.c to object file

--- drivers/build.sh
+++ drivers/build.sh
@@ -1,2 +1,2 @@
-#
+clang -m32 -fno-stack-protector -ffreestanding -fno-pie -c io/io.c -o ./bin/io.o
 clang -m32 -fno-stack-protector -ffreestanding -fno-pie -c vga/vga_textmode.c -o ./bin/vga_textmode.o
